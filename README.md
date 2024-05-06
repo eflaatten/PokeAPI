b2c4a60bc61af0558dfa08577b477d653fa3f780
eflaatten <elliotflaatten@live.com>
2024-05-06 12:06:09 -0500

    update commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PokeAPI
+
+Pokemon Collection
+
+[https://www.pokeapi.co/](https://pokeapi.co/)
+
+Sign up for api key
+Create an arena that displays two Pokemon battling each other.
+Compare the two to see who is likely to win.
+
+# PokeAPI
+Pokemon Collection
+[https://www.pokeapi.co/](https://pokeapi.co/)
+Sign up for api key
+Create an arena that displays two Pokemon battling each other.
+Compare the two to see who is likely to win.
+
+## Instructions
+1. Clone the repository.
+2. Install the required dependencies.
+3. Obtain an API key from [https://pokeapi.co/](https://pokeapi.co/).
+4. Run the application and enjoy the Pokemon battle!
+
--- main.js
+++ main.js
@@ -1,38 +1,49 @@
-// https://pokeapi.co/api/v2/{endpoint}/
 
-const getPokemon = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon/')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const pokemon = data.results;
-    const pokemonList = document.getElementById('pokemon-list');
-    pokemon.forEach(pokemon => {
-      const listItem = document.createElement('button');
-      listItem.innerText = pokemon.name;
-      pokemonList.appendChild(listItem);
+
+// USE POKEAPI TO GET 2 POKEMON AND DISPLAY THEM ON THE SCREEN
+const getPokemon = async (id) => {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    if(!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const data = await response.json()
+    console.log(data)
+
+    // get 2 random pokemon at a time
+    const randomPokemon = data.results.sort(() => 0.5 - Math.random()).slice(0, 2);
+
+    randomPokemon.forEach(pokemon => {
+      console.log(pokemon.name);
+      let pokemonElement = document.createElement('div');
+      pokemonElement.innerHTML = pokemon.name;
+      document.body.appendChild(pokemonElement);
     });
-  })
-}
 
-const chooseYourPokemon = () => {
-  const pokemonList = document.getElementById('pokemon-list');
-  pokemonList.innerHTML = '';
-  getPokemon();
-  pokemonList.addEventListener('click', event => {
-    const selectedPokemon = event.target.innerText;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      });
-  });
-}
+    // get the abilties of both pokemon
+    const pokemon1 = await fetch(randomPokemon[0].url)
+    const pokemon2 = await fetch(randomPokemon[1].url)
 
-const fight = () => {
-  const fightButton = document.getElementById('fight');
-  fightButton.addEventListener('click', () => {
-    chooseYourPokemon();
-  });
-}
+    const pokemon1Data = await pokemon1.json();
+    console.log(pokemon1Data.name, " has abilities: ", pokemon1Data.abilities.map(ability => ability.ability.name).join(", "));
 
+    const pokemon2Data = await pokemon2.json();
+    console.log(pokemon2Data.name, " has abilities: ", pokemon2Data.abilities.map(ability => ability.ability.name).join(", "));
+
+    pokemon2Data.abilities.forEach(ability => {
+      const abilityButton = document.createElement('button');
+      abilityButton.innerHTML = ability.ability.name;
+      document.body.appendChild(abilityButton);
+    });
+
+    pokemon1Data.abilities.forEach(ability => {
+      const abilityButton = document.createElement('button');
+      abilityButton.innerHTML = ability.ability.name;
+      document.body.appendChild(abilityButton);
+    });
+
+
+  } catch (error){ 
+    console.log("error getting pokemon")
+  }
+}
